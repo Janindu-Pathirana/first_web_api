d41005e3e691487e9020ef9b2d186237129c0136
Janindu Pathirana <janindup@99x.io>
2022-08-23 09:03:24 +0530

    add first file and git ignore file

--- first_Webapi/firstWeb.cs
+++ first_Webapi/firstWeb.cs
@@ -2,5 +2,9 @@
 {
     public class firstWeb
     {
+        public int id { get; set; }
+        public string name { get; set; } = string.Empty;
+        public int age { get; set; }
+        public string schoolName { get; set; } = string.Empty;
     }
 }
