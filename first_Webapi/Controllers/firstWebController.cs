f46c278d3b8128e8acc052bd865dc5a95165b117
Janindu Pathirana <janindup@99x.io>
2022-08-23 09:21:16 +0530

    add controler class

---
+++ first_Webapi/Controllers/firstWebController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace first_Webapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class firstWebController : ControllerBase
+    {
+    }
+}
